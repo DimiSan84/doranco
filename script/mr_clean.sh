64eb5b2dbde01cc78387c23f3076d05b892db3c6
dimi_san <58069646+DimiSan84@users.noreply.github.com>
2022-09-23 18:32:56 +0200

    Delete script directory

--- script/mr_clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-rm *~
-
-echo "J'ai supp tout les fichier avec ~"
--- script/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "bonjour tout le monde"
-mkdir -p /home/{Albert/{Images,Compta/{2011,2012,2013,2014}},Thierry/{Photos,Travail/{Client1,Client2,Client3}}} && touch /home/{Albert/{Image,Compta/{2011/"2011.txt",2012/"2012.txt",2013/"2013.txt",2014/"2014.txt"}},Thierry/{Photos,Travail/{Client1/"Cli1",Client2/"Cli2",Client3/"Cli3"}}}
-echo "réussi"
